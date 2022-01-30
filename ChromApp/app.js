62e2229b9d9bc7939375f7dd43b27e4e070b24ee
KyoungHyunPark <findkh@nate.com>
2022-01-30 22:24:36 +0900

    220130

--- ChromApp/app.js
+++ ChromApp/app.js
@@ -1,6 +1,14 @@
-function hello(name, age) {
-    console.log(name ,age);
+//숙제
+const calculator = {
+    add : function(a, b) { console.log(a + b); },
+    minus : function(a, b) { console.log(a - b); },
+    multiple : function(a, b) { console.log(a * b);},
+    divide : function (a, b) { console.log(a / b);},
+    squareRoot : function(a, b) { console.log(a ** b);}
 }
 
-
-hello("뭐야?", 123);
+calculator.add(3, 6);
+calculator.minus(6, 3);
+calculator.multiple(3, 6);
+calculator.divide(6, 3);
+calculator.squareRoot(6, 3);
