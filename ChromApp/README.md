a5c8546b44aa6a475161024a8afe40a2dfde6120
KyoungHyunPark <findkh@nate.com>
2022-01-31 19:34:16 +0900

    220131

--- ChromApp/README.md
+++ ChromApp/README.md
@@ -1,13 +1,24 @@
-# 😀 설맞이 개인 프로젝트 - 바닐라JS로 크롬 app 만들기  
+# 😀 설맞이 개인 프로젝트 - 바닐라 JS로 크롬 app 만들기  
   
-- 220130 : JS 이론 정리  
+## 220130 : JS 이론 정리🤣
     - Basic data type  
     - Variables  
     - const and let  
-    - Booleans  
+    - Boolean  
     - Array  
     - Object  
-    - Function
-    
+    - Function  
 
+## 220131 : JS 이론 정리, CSS 적용하여 Login 기능 추가😤
+    - return  
+    - 반복문  
+    - Object  
+    - HTML element
+    - Event 처리
+    - querySelector 사용
+    - Login 기능
+    - localStorage 활용 - 사용자 이름 기억하기
+    학원에서 배운 이론이 반복되어 처음엔 흥미가 좀 떨어졌지만, 복습이라 생각하고 열심히 따라 했다. 잊고 있던 부분, 헷갈렸던 부분을 확실히 잡을 수 있었다.
+    자바 스크립트 함수와 이벤트 처리 부분을 좀 더 집중해서 들었다.😁
+    특히 localStorage 부분은 3개월 전에 언급만 하고 넘어갔던 부분이라 좋았다.
 
--- ChromApp/app.js
+++ ChromApp/app.js
@@ -1,14 +1,42 @@
-//숙제
-const calculator = {
-    add : function(a, b) { console.log(a + b); },
-    minus : function(a, b) { console.log(a - b); },
-    multiple : function(a, b) { console.log(a * b);},
-    divide : function (a, b) { console.log(a / b);},
-    squareRoot : function(a, b) { console.log(a ** b);}
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+// const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden"; //변수 이름을 대문자로 쓴느 이유 : String만 포함된 변수는 대문자로 표기, String을 저장하고 싶을 때 사용
+const USERNAME_KEY = "username";
+
+const savedUsername = localStorage.getItem(USERNAME_KEY); //username이 없으면 null을 리턴함
+
+if(savedUsername === null) { //username이 null이라면
+    //show the form 
+    loginForm.classList.remove(HIDDEN_CLASSNAME); //loginForm 화면에 보이게 함
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else { //localStorage에 이름이 저장되어 있다면
+    // show the greetings
+    // onLoginSubmit();
+    paintGreetings(savedUsername); //인사말을 출력한다.
 }
 
-calculator.add(3, 6);
-calculator.minus(6, 3);
-calculator.multiple(3, 6);
-calculator.divide(6, 3);
-calculator.squareRoot(6, 3);
+//username을 기억하는 funciton
+function onLoginSubmit(event) {
+    event.preventDefault(); //브라우저의 동작을 막음 (새로 고침)
+    loginForm.classList.add(HIDDEN_CLASSNAME); //Form을 숨겨줌
+    const username = loginInput.value; //input value를 username에 저장함
+    // console.log(username);
+    localStorage.setItem(USERNAME_KEY, username); //localStorage에 key-value값으로 저장함
+    paintGreetings(username); //인사말을 출력하는 function 호출
+}
+
+//인사말하는 function
+function paintGreetings (username) {
+    greeting.innerText = `"Hello ${username}`; //Hello 이름 출력
+    greeting.classList.remove(HIDDEN_CLASSNAME); //h1의 hidden 클래스명 제거함
+}
+
+// link.addEventListener("click", handleLinkClick)
+
+// function handleLinkClick(event) {
+    //     event.preventDefault();
+    //     console.dir(event);
+    //     alert("clicked!")
+    // }
